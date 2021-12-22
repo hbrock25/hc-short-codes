0e6b6b0730292d34fc4c5be63044384c5072828b
Hugh Brock <hbrock@gmail.com>
2021-12-22 11:54:39 -0500

    Reduce diff with deprecated pmpro-hc-short-codes plugin

--- hc-short-codes.php
+++ hc-short-codes.php
@@ -56,37 +56,6 @@ function pmpro_expiration_date_shortcode( $atts ) {
 
 add_shortcode('pmpro_expiration_date', 'pmpro_expiration_date_shortcode');
 
-/*
-   Shortcode to return: 
- ** text "notloggedin" if user is not logged in
- ** text "loggedin" if user is logged in but not a current member
- ** text "member" if user is a current member
- */
-
-function hc_login_conditional_text_shortcode ($atts) {
-    
-    $a = shortcode_atts( array('notloggedin' => '', 'loggedin' => '', 'member' => '',), $atts, 'hc_login_conditional_text');
-    
-    $user_id = get_current_user_id();
-
-    //no user ID? bail
-    if(!$user_id)
-	return $a['notloggedin'];
-
-    //make sure PMPro is active, if not return "logged in" string
-    if(!function_exists('pmpro_getMembershipLevelForUser'))
-	return $a['loggedin'];
-
-    //get the user's level
-    $level = pmpro_getMembershipLevelForUser($user_id);
-
-    if(!empty($level) && ($level->id == 1 || $level->id == 17))
-	return $a['member'];
-    else
-	return $a['loggedin'];
-
-}
-add_shortcode('hc_login_conditional_text', 'hc_login_conditional_text_shortcode');
 
 /*
    Shortcode to show membership account information
@@ -390,3 +359,35 @@ function pmpro_status_widget() {
 }
 
 add_shortcode('hc_pmpro_status_widget', 'pmpro_status_widget');
+
+/*
+   Shortcode to return: 
+ ** text "notloggedin" if user is not logged in
+ ** text "loggedin" if user is logged in but not a current member
+ ** text "member" if user is a current member
+ */
+
+function hc_login_conditional_text_shortcode ($atts) {
+    
+    $a = shortcode_atts( array('notloggedin' => '', 'loggedin' => '', 'member' => '',), $atts, 'hc_login_conditional_text');
+    
+    $user_id = get_current_user_id();
+
+    //no user ID? bail
+    if(!$user_id)
+	return $a['notloggedin'];
+
+    //make sure PMPro is active, if not return "logged in" string
+    if(!function_exists('pmpro_getMembershipLevelForUser'))
+	return $a['loggedin'];
+
+    //get the user's level
+    $level = pmpro_getMembershipLevelForUser($user_id);
+
+    if(!empty($level) && ($level->id == 1 || $level->id == 17))
+	return $a['member'];
+    else
+	return $a['loggedin'];
+
+}
+add_shortcode('hc_login_conditional_text', 'hc_login_conditional_text_shortcode');
